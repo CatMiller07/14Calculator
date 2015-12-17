f277664a18838fe0c5c6340bafef945d661c3d87
catmiller07 <catmiller07@comcast.net>
2015-12-16 20:22:39 -0500

    second commit

--- index.js
+++ index.js
@@ -1,75 +1,68 @@
 $(document).ready(function(){
 	var temp ="";
-	var do_1x = "";
-	var factor1="";
-	var factor2="";
+	var factors =[];
 	var  answer =0;
 	var  operation="";
 	
 	$("#AC").on("click",function(){
-		  $("input").val("");
-		  factor1 = "";
-		  factor2 = "";
+		  $("#calc-fld").val("");
+		  factors = [];
 		  temp = "";
 	});
 	
 	$("#CE").on("click",function(){
-		  $("input").val("");
-		  if (factor1 && !factor2) {
-			   factor1 = "";
-		  }
-		   if (factor1 &&factor2) {
-			   factor2 = "";
-		  }
+		  $("#calc-fld").val("");
+		  temp = "";
 		  
 	});
 	
 	$(".number").on("click",function(){	
-		temp += $(this).data("numval") ;
 		
-		//console.log("input number is " + temp )
-		$("input").val(temp);
+		temp += $(this).data("numval") ;
+		console.log("input number is " + temp );
+		$("#calc-fld").html(temp);
+		
 	});
 	
 	$(".opcode").on("click",function(){
-		 do_1x =true;
+		
+		 factors.push( Number(temp) );
 		 operation = $(this).data("oper") ;
+		 $("#calc-fld").html("");
+		 temp ="";
 		 //console.log("the operation is "+ operation);
     });	
 	
-		if ( factor1==="") {
-			factor1 = Number(temp);
-			console.log("factor1= "+temp);
-			temp = "";
-			$("input").val("");
-			
-		}
-		else
-			if (factor2==="") {
-				factor2= Number(temp);
-				temp = "";
-				$("input").val("");
-			};
 	
 		
 	$("#EQ").click(function(){
-		console.log("factor1= "+factor1 +" and factor2= " +factor2+ " and the opcode is " +operation );
+		factors.push( Number(temp) );
+		console.log("factors = "+factors + " and the opcode is " +operation );
 	
-			if (operation = "X") {
-				answer = factor1 * factor2;
+			if (operation === "X") {
+				answer = factors.reduce(function(a,b){
+					  return a * b;
+				});
 				console.log("factor1 * factor2=" +answer);
-			} else if (operation = "/") {
-				answer = Math.round(factor1 / factor2, 2) ;
+			} else if (operation === "/") {
+				answer = factors.reduce(function(a,b){
+					 return (a/b).toFixed(2);
+				});
+				
 				console.log("factor1/factor2 = "+answer);
-			} else if (operation = "+"){
-				answer = factor1 + factor2 ;
+			} else if (operation === "+"){
+				answer = factors.reduce(function(a,b){
+					 return a+b;
+				});
 				console.log("factor1 + factor2= "+answer);
 			} else{
-				answer = factor1 - factor2;
+				answer = factors.reduce(function(a,b){
+					 return a-b;
+				});
 				console.log("factor1 - factor2= "+answer);
 			};
 			
-			$("input").val(answer);
+			$("#calc-fld").val(answer);
 				
 	}); 
 		
